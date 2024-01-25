4b98f2d403732047011c8fb936e9a90b10344d00
KyryloChernovUptime <kyrylo.chernov@uptimedevelopment.pl>
2024-01-25 13:45:41 +0200

    Initial Commit

---
+++ ChatTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatTestApp
+//
+//  Created by Kyrylo Chernov on 25.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
